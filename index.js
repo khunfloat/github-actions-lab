bbc198e65a68bf1109988aee4accda6f97092b38
khunfloat <60844731+khunfloat@users.noreply.github.com>
2025-05-23 20:23:33 +0800

    feat(api): /yo

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World! I'm the master");
 });
 
+app.get("/yo", (req, res) => {
+  res.send("yoyoyo");
+});
+
 // yaya
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
